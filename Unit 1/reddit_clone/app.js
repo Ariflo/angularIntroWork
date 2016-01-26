10298fc28b31a3ed209efd0dea2d9f1492819481
AriFlo <ariflo@arianflores.com>
2016-01-26 15:43:49 -0800

    added sorting ability and search

--- Unit 1/reddit_clone/app.js
+++ Unit 1/reddit_clone/app.js
@@ -2,38 +2,26 @@ angular.module('myApp', ['ngAnimate']).controller('firstController',function($sc
 
 	$scope.posts = []; 
 	$scope.newPostData = {};
-	$scope.show = false; 	
-
+	$scope.show = false;
+	
 	$scope.postSubmit = function(form){
 		$scope.show = true;
+		$scope.number = 0;
 		if (form.$valid) {
-			$scope.posts.push({"title": $scope.newPostData.title, "author": $scope.newPostData.author, "image": $scope.newPostData.image, "comment": $scope.newPostData.comment, "date": new Date() });
+			$scope.posts.push({"title": $scope.newPostData.title, "author": $scope.newPostData.author, "image": $scope.newPostData.image, "comment": $scope.newPostData.comment, "date": new Date(), "rating": $scope.number});
 
 		};
 		$scope.newPostData = {};
 	}
 
 	
-	$scope.number = 0; 
-	$scope.ratingUp = function(){
-		$scope.number += 1;
+	$scope.ratingUp = function(post){
+		post.rating += 1;
 	}
 
-	$scope.ratingDown = function(){
-		$scope.number -= 1;
+	$scope.ratingDown = function(post){
+		post.rating -= 1;
 	}
 
-	$scope.rating = false;
-	$scope.votesClicked = function(){
-		$scope.rating = !$scope.rating
-	}	
-
-	$scope.dateClicked = function(){
-		$scope.rating = !$scope.rating
-	}	
-
-	$scope.titleClicked = function(){
-		$scope.rating = !$scope.rating
-	}
 });
 
