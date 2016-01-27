e0fbd4ede5f38663a639ead32a8793628e2ac0a8
AriFlo <ariflo@arianflores.com>
2016-01-26 16:52:58 -0800

    added validation to forms

--- Unit 1/reddit_clone/app.js
+++ Unit 1/reddit_clone/app.js
@@ -3,23 +3,16 @@ angular.module('myApp', ['ngAnimate']).controller('firstController',function($sc
 	$scope.posts = []; 
 	$scope.newPostData = {};
 	$scope.show = false;
-<<<<<<< HEAD
-	
-=======
+
 	$scope.commentOn = false;
 	$scope.newCommentOn = false;
 
->>>>>>> Comments added
+
 	$scope.postSubmit = function(form){
 		$scope.show = true;
-		$scope.number = 0;
 		if (form.$valid) {
-<<<<<<< HEAD
-			$scope.posts.push({"title": $scope.newPostData.title, "author": $scope.newPostData.author, "image": $scope.newPostData.image, "comment": $scope.newPostData.comment, "date": new Date(), "rating": $scope.number});
-=======
-			$scope.posts.push({"title": $scope.newPostData.title, "author": $scope.newPostData.author, "image": $scope.newPostData.image, "comment": $scope.newPostData.comment, "date": new Date(), "comments": [], "addComment": {}});
->>>>>>> Comments added
-
+			$scope.number = 0;
+			$scope.posts.push({"title": $scope.newPostData.title, "author": $scope.newPostData.author, "image": $scope.newPostData.image, "comment": $scope.newPostData.comment, "date": new Date(), "rating": $scope.number, "comments": [], "addComment": {}});
 		};
 		$scope.newPostData = {};
 	}
@@ -42,30 +35,12 @@ angular.module('myApp', ['ngAnimate']).controller('firstController',function($sc
 		post.rating -= 1;
 	}
 
-<<<<<<< HEAD
-=======
-	$scope.rating = false;
-	$scope.votesClicked = function(){
-		$scope.rating = !$scope.rating
-	}	
-
-	$scope.dateClicked = function(){
-		$scope.rating = !$scope.rating
-	}	
-
-	$scope.titleClicked = function(){
-		$scope.rating = !$scope.rating
-	}
-
 	$scope.toggleComments = function() {
-		console.log("it's firin'");
 		$scope.commentOn= !$scope.commentOn;
 	}
 
 	$scope.toggleNewComment = function() {
-		console.log("This is also a-firin'");
 		$scope.newCommentOn = !$scope.newCommentOn;
 	}
->>>>>>> Comments added
 });
 
