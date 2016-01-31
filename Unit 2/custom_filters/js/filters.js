246d95382cf40610a2d7dec502c29c2e433d46a3
AriFlo <ariflo@arianflores.com>
2016-01-31 11:06:38 -0800

    created custom filter

--- Unit 2/custom_filters/js/filters.js
+++ Unit 2/custom_filters/js/filters.js
@@ -1,3 +1,16 @@
 filterApp.filter('kebab', function () {
+	return function (input) {
+		return input.replace(/_/g, '-');		
+  	};
+});
 
+filterApp.filter('camel', function () {
+	return function (input) {
+		for(var i = 0; i<input.length; i++){
+			if(input[i] === '_' || input[i] === '-'){
+				var letter = input[i + 1].toUpperCase();
+				return input.replace(input[i+1], letter).replace(/-/g, '').replace(/_/g, '');
+			}
+		}		
+  	};
 });
