97e14c176241ac3389ff5075bf968de7646b9080
AriFlo <ariflo@arianflores.com>
2016-01-31 16:40:03 -0800

    worked up to query

--- Unit 2/Routing/routeExample3/js/app.js
+++ Unit 2/Routing/routeExample3/js/app.js
@@ -1,10 +1,17 @@
 var routingApp = angular.module('routingApp', ['ngRoute']);
 
-routingApp.config(function($routeProvider) {
-    $routeProvider     
+routingApp.config(function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode( true );
+    $routeProvider 
+
       .when('/add/:num1/:num2', {
         templateUrl: 'partials/add.html',
         controller: 'calculateController'
+      })  
+
+      .when('/add/?x=4&y=3', {
+        templateUrl: 'partials/query.html',
+        controller: 'calculateQueryController'
       })
 
       .when('/div/:num1/:num2', {
--- Unit 2/Routing/routeExample3/js/controllers.js
+++ Unit 2/Routing/routeExample3/js/controllers.js
@@ -2,6 +2,10 @@ routingApp.controller('calculateController',  ['$scope', '$routeParams', functio
   $scope.sum = parseInt($routeParams.num1) + parseInt($routeParams.num2); 
 }]);
 
+routingApp.controller('calculateQueryController',  ['$scope','$location', function($scope, $location) {
+	console.log($location.search());
+}]);
+
 routingApp.controller('divController',  ['$scope', '$routeParams', function($scope, $routeParams) {
   $scope.quote = parseInt($routeParams.num1) / parseInt($routeParams.num2); ;
 }]);
